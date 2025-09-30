3356b857875b44f807a5166ba2593ea6048671d1
taiwotee12 <taiwotee43@gmail.com>
2025-09-30 08:01:55 -0400

    added index.php file

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-echo "Hello, World!";
+<?php
+session_start();
+
+// Check if the user is already logged in, if yes then redirect to dashboard
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: dashboard.php");
+    exit;
+} else {
+    // If not logged in, redirect to login page
+    header("location: login.php");
+    exit;
+}
+?>
